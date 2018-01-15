cdb7e0d0ceb60a9356965c8730a0a4210747ac49
Matt McGrath <mcm223@g.harvard.edu>
2018-01-14 19:07:21 -0800

    Add averageArray function

--- monitor.py
+++ monitor.py
@@ -1,9 +1,14 @@
 import Adafruit_DHT
 import time
 from array import array
+from functools import reduce
 
-# Adafruit variables
+# Global variables
 sensor = Adafruit_DHT.AM2302
+todaysHigh = 0.0
+todaysLow = 0.0
+avgTemp = 0.0
+avgHum = 0.0
 
 # GPIO Input
 # Really pin 7, but GPIO 4
@@ -13,25 +18,36 @@ pin = 4
 tempArr = array('f',[])
 humArr = array('f',[])
 
+def averageArray(input):
+    sum = reduce((lambda x, y: x + y), input)
+    avg = sum / len(input)
+    return avg
+
+# Continuously get readings and keep five values in array for averaging to smooth outliers
 for x in range(0,10):
     # Get reading
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-    # Convert temp to Fahrenheit
+    # Sensor may occasionally reject read
     if humidity is not None and temperature is not None:
+
+        # Convert to fahrenheit
         temperature = ((temperature * (9.0/5.0))+32.0)
+
+        # Process temperature
         if len(tempArr) == 5:
             tempArr.pop(0)
         tempArr.append(temperature)
+        avgTemp = averageArray(tempArr)
+
+        # Process humidity
         if len(humArr) == 5:
             humArr.pop(0)
         humArr.append(humidity)
-        print(tempArr)
-        # print(humArr)
+        avgHum = averageArray(humArr)
     else:
         print('Failed to get reading.')
     time.sleep(5)
-
 
 # print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
 
