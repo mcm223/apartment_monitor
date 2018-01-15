a3c9e9880a2d4a3598d16d4e72dc68fb989dc682
Matt McGrath <mcm223@g.harvard.edu>
2018-01-14 17:01:55 -0800

    Add script

---
+++ monitor.py
@@ -0,0 +1,18 @@
+import Adafruit_DHT
+
+# Adafruit variables
+sensor = Adafruit_DHT.AM2302
+
+# GPIO Input
+# Really pin 7, but GPIO 4
+pin = 4
+
+# Get reading
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+# Convert temp to Fahrenheit
+temperature = ((temperature * (9.0/5.0))+32.0)
+
+print(humidity)
+print(temperature)
+
