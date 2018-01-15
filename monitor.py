0ca1dba30d9174c6ed7d1a33eba38674f1548efc
Matt McGrath <mcm223@g.harvard.edu>
2018-01-14 17:13:20 -0800

    Add if statement

--- monitor.py
+++ monitor.py
@@ -11,7 +11,12 @@ pin = 4
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 # Convert temp to Fahrenheit
-temperature = ((temperature * (9.0/5.0))+32.0)
+if humidity is not None and temperature is not None:
+    temperature = ((temperature * (9.0/5.0))+32.0)
+else:
+    print('Failed to get reading.')
+    continue
+
 
 print(humidity)
 print(temperature)
